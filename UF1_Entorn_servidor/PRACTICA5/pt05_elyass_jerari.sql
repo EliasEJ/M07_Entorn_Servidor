9085b220d4787a65b7d8c5e6d3e0ddcbe4c2b9a1
EliasEJ <e.jerari@sapalomera.cat>
2023-11-12 23:22:54 +0100

    Modificació estructura bd

--- UF1_Entorn_servidor/PRACTICA5/pt05_elyass_jerari.sql
+++ UF1_Entorn_servidor/PRACTICA5/pt05_elyass_jerari.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-10-2023 a las 23:48:50
+-- Tiempo de generación: 12-11-2023 a las 23:21:42
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
--- Elyass Jerari
-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,14 +34,14 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `article` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `articles`
 --
 
 INSERT INTO `articles` (`id`, `article`) VALUES
-(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+(1, 'Lorem ipsum'),
 (2, 'Massa tincidunt nunc pulvinar sapien et ligula ullamcorper.'),
 (3, 'Ultrices tincidunt arcu non sodales neque sodales ut.'),
 (4, 'Mauris ultrices eros in cursus turpis massa tincidunt dui ut.'),
@@ -69,7 +67,8 @@ INSERT INTO `articles` (`id`, `article`) VALUES
 (24, 'Integer malesuada nunc vel risus. Non diam phasellus vestibulum lorem sed risus.'),
 (25, 'Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Feugiat scelerisque varius morbi enim. Ligula ullamcorper malesuada proin libero nunc.'),
 (26, 'Adipiscing at in tellus integer feugiat scelerisque varius.'),
-(27, 'Placerat in egestas erat imperdiet sed euismod nisi porta. A cras semper auctor neque vitae tempus quam pellentesque nec.');
+(27, 'Placerat in egestas erat imperdiet sed euismod nisi porta. A cras semper auctor neque vitae tempus quam pellentesque nec.'),
+(33, 'Lorem ipsum');
 
 -- --------------------------------------------------------
 
@@ -81,9 +80,36 @@ DROP TABLE IF EXISTS `usuaris`;
 CREATE TABLE IF NOT EXISTS `usuaris` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `token` varchar(190) DEFAULT NULL,
+  `google_id` varchar(150) DEFAULT NULL,
+  `github_id` varchar(150) DEFAULT NULL,
+  `token_creation_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuaris`
+--
+
+INSERT INTO `usuaris` (`id`, `username`, `password`, `token`, `google_id`, `github_id`, `token_creation_time`) VALUES
+(4, 'elyass909@gmail.com', '$2y$10$KPUqH8FSrv2vu8I.0dKQLuv8lqCJgbrKnqbp5B10dkMud8A.atSlW', '60d1f9de3b73cfae3c985bf031b0294a3c1d13df50407b6b5a2f058d0a8dba10', '', NULL, '2023-11-12 22:04:53'),
+(7, '\'elyass909@gmail.com\'', NULL, NULL, '\'116185564387212403809\'', NULL, '2023-11-12 22:04:29'),
+(8, '\'e.jerari@sapalomera.cat\'', NULL, NULL, '\'111578038167253596436\'', NULL, '2023-11-12 22:04:29'),
+(9, '\'elyasseljerari@institutescolalloretdemar.cat\'', NULL, NULL, '\'109743500783709889769\'', NULL, '2023-11-12 22:04:29');
+
+--
+-- Disparadores `usuaris`
+--
+DROP TRIGGER IF EXISTS `before_update_usuaris`;
+DELIMITER $$
+CREATE TRIGGER `before_update_usuaris` BEFORE UPDATE ON `usuaris` FOR EACH ROW BEGIN
+    IF NEW.token IS NOT NULL THEN
+        SET NEW.token_creation_time = IFNULL(NEW.token_creation_time, CURRENT_TIMESTAMP);
+    END IF;
+END
+$$
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
